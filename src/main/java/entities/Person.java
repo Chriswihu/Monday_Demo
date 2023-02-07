cc5e2056c57a4d733f8517799115fe22d40453f2
Chriswihu <chris.wihu@gmail.com>
2023-02-07 20:28:05 +0100

    update

--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -1,52 +1,110 @@
 package entities;
 
-import jakarta.persistence.*;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "person")
+@NamedQueries({
+        @NamedQuery(name = "Person.deleteById", query = "delete from Person p where p.id = :id")
+})
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 30)
     private String name;
+    @Column(name = "birth_date", nullable = false)
+//    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime birthDate;
 
-    @Column(name = "age", nullable = false)
+    @Transient
     private Integer age;
 
     @ElementCollection
     @Column(name = "address")
     @CollectionTable(name = "person_addresses", joinColumns = @JoinColumn(name = "owner_id"))
-    private Set<String> addresses = new LinkedHashSet<>();
+    private Set<String> addresses = new HashSet<>();
 
+    @OneToMany(mappedBy = "person", orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<Phone> phones = new LinkedHashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "person_cars",
+            joinColumns = @JoinColumn(name = "person_id"),
+            inverseJoinColumns = @JoinColumn(name = "cars_id"))
+    private Set<Car> cars = new LinkedHashSet<>();
+
+    @Column(name = "created", updatable = false)
+    private LocalDateTime created;
+
+    @Column(name = "modified")
+    private LocalDateTime editted;
+
+    // Database is not set to handle dates, so I do it with JPA lifecycle methods. For more see: https://www.baeldung.com/jpa-entity-lifecycle-events
+    @PreUpdate
+    public void onUpdate() {
+        editted = LocalDateTime.now(ZoneId.of("GMT+01:00"));
+    }
+
+    @PrePersist
+    public void onPersist(){
+        editted = LocalDateTime.now(ZoneId.of("GMT+01:00"));
+        created = LocalDateTime.now(ZoneId.of("GMT+01:00"));
+    }
+
+    public Person() {
+    }
+    public Person(String name) {
+        this.name = name;
+    }
+    public Person(String name, String birthDate) {
+        this.name = name;
+        setBirthDate(String.valueOf(birthDate));
+        setAge();
+    }
     public Set<String> getAddresses() {
         return addresses;
     }
 
-    public void setAddresses(Set<String> addresses) {
-        this.addresses = addresses;
+    public void addAddress(String address) {
+        this.addresses.add(address);
     }
 
-    public Person(){
-
+    public void removeAddress(String address) {
+        for (String a : this.addresses)
+            if (a == address)
+                this.addresses.remove(a);
     }
-    public Person(String name, Integer age){
-        this.name = name;
-        this.age = age;
+    public Set<Car> getCars() {
+        return cars;
+    }
 
+    public void addCar(Car car) {
+        this.cars.add(car);
+        car.getPersons().add(this);
+    }
+    public void removeCar(Car car) {
+        this.cars.remove(car);
+        car.getPersons().remove(this);
     }
 
     public Integer getAge() {
         return age;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setAge( ) {
+        java.time.Duration duration = java.time.Duration.between(this.birthDate, LocalDateTime.now());
+        this.age = (int) duration.toDays() / 365;
     }
 
     public String getName() {
@@ -57,6 +115,20 @@ public class Person {
         this.name = name;
     }
 
+    public Set<Phone> getPhones() {
+        return phones;
+    }
+
+    public void addPhone(Phone phone) {
+        this.phones.add(phone);
+        phone.setPerson(this);
+    }
+
+    public void removePhone(Phone phone) {
+        this.phones.remove(phone);
+        phone.setPerson(null);
+    }
+
     public Long getId() {
         return id;
     }
@@ -65,11 +137,23 @@ public class Person {
         this.id = id;
     }
 
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime bd = LocalDateTime.parse(birthDate, formatter);
+        this.birthDate = bd;
+    }
+
     @Override
     public String toString() {
         return getClass().getSimpleName() + "(" +
                 "id = " + id + ", " +
                 "name = " + name + ", " +
-                "age = " + age + ")";
+                "birthDate = " + birthDate + ", " +
+                "age = " + this.age + ", " +
+                "addresses = " + addresses + ")";
     }
 }
--- src/main/java/facade/PersonFacade.java
+++ src/main/java/facade/PersonFacade.java
@@ -1,7 +1,9 @@
 package facade;
 
+import entities.Car;
 import entities.Person;
 
+import entities.Phone;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -10,12 +12,17 @@ import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 public class PersonFacade {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    private jakarta.persistence.EntityManager entityManager;
-    public Person createPerson(Person p){
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+    public Person createPerson(Person p) {
         EntityManager em = emf.createEntityManager();
-        try{
+        try {
             em.getTransaction().begin();
+            p.getCars().forEach(car -> {
+                if(car.getId()==null)
+                    em.persist(car);
+                else
+                    em.merge(car);
+            });
             em.persist(p);
             em.getTransaction().commit();
         } finally {
@@ -23,48 +30,39 @@ public class PersonFacade {
         }
         return p;
     }
-
     public List<Person> getAllPersons(){
         EntityManager em = emf.createEntityManager();
-        TypedQuery<Person> tq = em.createQuery("SELECT p from Person p", Person.class);
+        TypedQuery<Person> tq = em.createQuery("SELECT p FROM Person p", Person.class);
         List<Person> persons = tq.getResultList();
         return persons;
     }
-
     public Person getPerson(Long id){
         EntityManager em = emf.createEntityManager();
         Person p = em.find(Person.class, id);
         return p;
     }
-
-
-
     public Person updatePerson(Person p){
-        Person result = null;
         EntityManager em = emf.createEntityManager();
         Person found = em.find(Person.class, p.getId());
-        if(found == null){
-            throw new IllegalArgumentException("No Person with this ID: ");
+        if (found == null) {
+            throw new IllegalArgumentException("No person with that id");
         }
-        try{
+        try {
             em.getTransaction().begin();
-            result = em.merge(p);
+            em.merge(p);
             em.getTransaction().commit();
         } finally {
             em.close();
         }
-        System.out.println("BEFORE UPDATE: " + p);
-        System.out.println("AFTER UPDATE: " + result);
-        return result;
+        return p;
     }
-
     public Person deletePerson(Long id){
         EntityManager em = emf.createEntityManager();
         Person p = em.find(Person.class, id);
-        if(p == null){
-            throw new IllegalArgumentException("No Person with this ID: ");
+        if (p == null) {
+            throw new IllegalArgumentException("No person with that id: " + id + " exists");
         }
-        try{
+        try {
             em.getTransaction().begin();
             em.remove(p);
             em.getTransaction().commit();
@@ -74,23 +72,32 @@ public class PersonFacade {
         return p;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         PersonFacade pf = new PersonFacade();
-//        Person newbie = new Person("Niko", 25);
-//        pf.createPerson(newbie);
-//        System.out.println("The person gets this new id: " + newbie.getId());
-        System.out.println("Before Any Action");
-        pf.getAllPersons().forEach((p)-> System.out.println(p));
-//        System.out.println("GET SINGLE PERSON BY ID: ");
-//        System.out.println(pf.getPerson(1L));
+        Person p = new Person("Jesper", "1968-01-01 00:00");
+        p.addAddress("Hovedgaden 1");
+        Phone phone = new Phone("12345678", "Home");
+        p.addPhone(phone);
+        p.addCar(new Car("Volvo", "V70", 2000));
+        pf.createPerson(p);
+//        System.out.println("The person got this new id: " + p.getId());
+//        pf.getAllPersons().forEach((person)-> System.out.println(person));
+//        System.out.println("GET SINGLE PERSON BY ID:");
 //        Person p = pf.getPerson(1L);
 //        System.out.println(p);
-//        p.setName("Chris");
+//        p.setName("Jannie");
 //        pf.updatePerson(p);
-        pf.deletePerson(4L);
-        System.out.println("After Delete");
+//        System.out.println("");
+//        pf.getAllPersons().forEach((person)-> System.out.println(person));
+//        System.out.println("DELETE PERSON WITH ID 1");
+//        pf.deletePerson(1L);
         pf.getAllPersons().forEach((person)-> System.out.println(person));
-
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        TypedQuery<Person> tq = em.createNamedQuery("Person.deleteById", Person.class);
+        tq.setParameter("id", 1L);
+        tq.executeUpdate();
+        em.getTransaction().commit();
+        em.close();
     }
 }
